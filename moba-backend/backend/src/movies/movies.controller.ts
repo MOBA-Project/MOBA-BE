15192fa1b82854b3e0d7eaf21bd5b0eaa628cf7d
hskhsmm <hskhsmm@naver.com>
2025-10-14 22:31:17 +0900

    feat: MoviesService를 실제 HTTP 요청으로 사용

--- moba-backend/backend/src/movies/movies.controller.ts
+++ moba-backend/backend/src/movies/movies.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getMovies(@Query('page') page?: number, @Query('genre') genre?: string) {
+    return this.moviesService.getMovies(page, genre);
+  }
+
+  @Get(':id')
+  getMovieDetail(@Param('id') id: string) {
+    return this.moviesService.getMovieDetail(id);
+  }
+
+  @Get(':id/videos')
+  getMovieVideos(@Param('id') id: string) {
+    return this.moviesService.getMovieVideos(id);
+  }
+
+  @Get('search/query')
+  searchMovies(@Query('query') query: string, @Query('page') page?: number) {
+    return this.moviesService.searchMovies(query, page);
+  }
+}
