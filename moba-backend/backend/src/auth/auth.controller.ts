55218511310b9633f231d8ecc43f580cb62163d4
hskhsmm <hskhsmm@naver.com>
2025-10-15 15:20:57 +0900

    feat: Auth 컨트롤러 swagger탭 추가

--- moba-backend/backend/src/auth/auth.controller.ts
+++ moba-backend/backend/src/auth/auth.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  // 다음 단계에서 signup, login 라우트 추가 예정
+}
