c19676cc6f58b8181948f9c24de2993336af3b33
leepg038292 <leepg038292@naver.com>
2025-10-16 14:41:37 +0900

    feat :  Reviews Module 설정 및 App Module에 등록

--- moba-backend/backend/src/app.module.ts
+++ moba-backend/backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
 import { AuthModule } from './auth/auth.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     MoviesModule,
     AuthModule,
+    ReviewsModule,
   ],
 })
 export class AppModule {}
