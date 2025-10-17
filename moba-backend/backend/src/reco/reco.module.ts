945edf80086a5528db8843ec9e64eb21e1d3d09f
leepg038292 <leepg038292@naver.com>
2025-10-17 16:16:31 +0900

    feat : reco 모듈 수정

--- moba-backend/backend/src/reco/reco.module.ts
+++ moba-backend/backend/src/reco/reco.module.ts
@@ -3,15 +3,23 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RecoController } from './reco.controller';
 import { RecoService } from './reco.service';
 import { UserProfile, UserProfileSchema } from './schemas/user-profile.schema';
+import { IngestController } from './ingest.controller';
+import { IngestService } from './ingest.service';
+import { Movie, MovieSchema } from '../movies/schemas/movie.schema';
+import { MovieVector, MovieVectorSchema } from './schemas/movie-vector.schema';
+import { VectorMeta, VectorMetaSchema, VectorTerm, VectorTermSchema } from './schemas/vector-term.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: UserProfile.name, schema: UserProfileSchema },
+      { name: Movie.name, schema: MovieSchema },
+      { name: MovieVector.name, schema: MovieVectorSchema },
+      { name: VectorTerm.name, schema: VectorTermSchema },
+      { name: VectorMeta.name, schema: VectorMetaSchema },
     ]),
   ],
-  controllers: [RecoController],
-  providers: [RecoService],
+  controllers: [RecoController, IngestController],
+  providers: [RecoService, IngestService],
 })
 export class RecoModule {}
-
