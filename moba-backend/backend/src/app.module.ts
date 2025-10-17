d9ca25c713a59d73e41ac6196e304fce23213d9f
hskhsmm <hskhsmm@naver.com>
2025-10-17 17:49:16 +0900

    feat:AppModule에 Posts와 Comments 모듈 등록

--- moba-backend/backend/src/app.module.ts
+++ moba-backend/backend/src/app.module.ts
@@ -5,6 +5,8 @@ import { MoviesModule } from './movies/movies.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { AuthModule } from './auth/auth.module';
 import { ReviewsModule } from './reviews/reviews.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -19,6 +21,8 @@ import { ReviewsModule } from './reviews/reviews.module';
     BookmarksModule,
     AuthModule,
     ReviewsModule,
+    PostsModule,
+    CommentsModule,
   ],
 })
 export class AppModule {}
